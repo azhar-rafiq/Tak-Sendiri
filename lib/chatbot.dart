911931eec46ef962a8a178adc1305db3e51e1fc1
Azhar <azhar@outlook.co.id>
2023-07-22 18:43:58 +0700

    UI Done

---
+++ lib/chatbot.dart
@@ -0,0 +1,267 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:convert';
+import 'chatMsg.dart';
+import 'package:linkify/linkify.dart';
+
+class chatbot extends StatefulWidget {
+  const chatbot({super.key});
+
+  final String title = "Chat: Koya";
+
+  @override
+  State<chatbot> createState() => chatbotState();
+}
+
+class chatbotState extends State<chatbot> {
+  @override
+  void initState() {}
+
+  int idUser = 1;
+  String name = "Budi";
+  String prompt = "Med Stud Games";
+  String newChat = "";
+
+  final textPrompt = TextEditingController();
+
+  List<ChatMessage> messages = [
+    ChatMessage(
+        messageContent:
+            "Hi! Nama ku Koya. Apa yang sedang kamu rasakan saat ini?",
+        messageType: "receiver"),
+  ];
+
+  final ScrollController _controller = ScrollController();
+
+  void _scrollDown() {
+    _controller.animateTo(
+      _controller.position.maxScrollExtent,
+      duration: Duration(seconds: 2),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  void promptTrigger() {
+    if (textPrompt.text == "") {
+    } else {
+      prompt = textPrompt.text;
+      setState(() {
+        newChat = prompt;
+        messages
+            .add(ChatMessage(messageContent: newChat, messageType: "sender"));
+
+        textPrompt.clear();
+      });
+      _scrollDown();
+      http
+          .post(Uri.parse("http://4.156.87.67/api/prompts"),
+              headers: {'Content-type': 'application/json'},
+              body: json.encode({"user_id": idUser, "prompt": prompt}))
+          .then((response) {
+        print(response.body);
+        // print("pokemon");
+        setState(() {
+          newChat = json.decode(response!.body)['response'];
+          messages.add(
+              ChatMessage(messageContent: newChat, messageType: "receiver"));
+
+          _scrollDown();
+        });
+
+        _scrollDown();
+      });
+    }
+  }
+
+  String extractLink(String input) {
+    var elements = linkify(input,
+        options: LinkifyOptions(
+          humanize: false,
+        ));
+    for (var e in elements) {
+      if (e is LinkableElement) {
+        return e.url;
+      }
+    }
+    return "no link";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Scrollbar(
+            child: ListView.builder(
+              controller: _controller,
+              itemCount: messages.length,
+              shrinkWrap: true,
+              padding: EdgeInsets.only(top: 10, bottom: 100),
+              // physics: NeverScrollableScrollPhysics(),
+              itemBuilder: (context, index) {
+                return Container(
+                  padding:
+                      EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
+                  child: Align(
+                    alignment: (messages[index].messageType == "receiver"
+                        ? Alignment.topLeft
+                        : Alignment.topRight),
+                    child: Row(children: <Widget>[
+                      Visibility(
+                        visible: messages[index].messageType == "receiver"
+                            ? true
+                            : false,
+                        child: Expanded(
+                          flex: 5,
+                          child: Image.asset('images/koya.png',
+                              color: Color.fromRGBO(255, 255, 255, 0.5),
+                              colorBlendMode: BlendMode.modulate,
+                              height: 40,
+                              width: 40),
+                        ),
+                      ),
+                      Expanded(flex: 1, child: Container()),
+                      Expanded(
+                        flex: 30,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius:
+                                (messages[index].messageType == "receiver"
+                                    ? BorderRadius.only(
+                                        topRight: Radius.circular(15.0),
+                                        bottomLeft: Radius.circular(25.0),
+                                        bottomRight: Radius.circular(15.0),
+                                      )
+                                    : BorderRadius.only(
+                                        topLeft: Radius.circular(15.0),
+                                        bottomLeft: Radius.circular(15.0),
+                                        bottomRight: Radius.circular(25.0),
+                                      )),
+                            color: (messages[index].messageType == "receiver"
+                                ? Colors.grey.shade200
+                                : Color.fromARGB(255, 47, 44, 80)),
+                          ),
+                          padding: EdgeInsets.all(16),
+                          child: extractLink(messages[index].messageContent) ==
+                                  "no link"
+                              ? Text(
+                                  messages[index].messageContent,
+                                  style: TextStyle(
+                                      fontSize: 15,
+                                      color: messages[index].messageType ==
+                                              "receiver"
+                                          ? Colors.black
+                                          : Colors.white),
+                                  textAlign:
+                                      messages[index].messageType == "receiver"
+                                          ? TextAlign.left
+                                          : TextAlign.right,
+                                )
+                              : InkWell(
+                                  child: Text(
+                                    messages[index].messageContent,
+                                    style: TextStyle(color: Colors.blue),
+                                  ),
+                                  onTap: () {
+                                    launchUrl(Uri.parse(extractLink(
+                                        messages[index].messageContent)));
+                                  },
+                                ),
+//                           Html(
+//                               data: messages[index].messageContent,
+
+//                               onLinkTap: (url, context, attributes, element) {
+//                                 // open url in a webview
+//                                 launchUrl(url! as Uri);
+//                               },
+// //
+//                               style: {
+//                                 "html": Style.fromTextStyle(
+//                                   TextStyle(fontSize: 14.0),
+//                                 ),
+//                               },
+//                             ),
+                        ),
+                      ),
+                    ]),
+                  ),
+                );
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Container(
+              padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
+              height: 60,
+              width: double.infinity,
+              color: Colors.white,
+              child: Row(
+                children: <Widget>[
+                  // GestureDetector(
+                  //   onTap: () {},
+                  //   child: Container(
+                  //     height: 30,
+                  //     width: 30,
+                  //     decoration: BoxDecoration(
+                  //       color: Color.fromARGB(255, 47, 44, 80),
+                  //       borderRadius: BorderRadius.circular(30),
+                  //     ),
+                  //     child: Icon(
+                  //       Icons.clear,
+                  //       color: Colors.white,
+                  //       size: 20,
+                  //     ),
+                  //   ),
+                  // ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Expanded(
+                    child: TextField(
+                      controller: textPrompt,
+                      decoration: InputDecoration(
+                          hintText: "Ketik di sini...",
+                          hintStyle: TextStyle(color: Colors.black54),
+                          border: InputBorder.none),
+                      onEditingComplete: () {
+                        promptTrigger();
+                        FocusScope.of(context).unfocus();
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      promptTrigger();
+                    },
+                    child: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                      size: 18,
+                    ),
+                    backgroundColor: Color.fromARGB(255, 47, 44, 80),
+                    elevation: 0,
+                    shape: CircleBorder(
+                        // borderRadius: BorderRadius.all(Radius.circular(15.0))
+                        ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:url_launcher/url_launcher.dart';
-// import 'dart:async';
-import 'dart:convert';
-import 'chatMsg.dart';
-import 'package:linkify/linkify.dart';
-// import 'package:flutter_html/flutter_html.dart';
+import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
+
+import 'package:sleeping_koala/chatbot.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,14 +14,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Tak Sendiri',
       theme: ThemeData(
         fontFamily: 'MavenPro',
         colorScheme: ColorScheme.fromSwatch(
-            backgroundColor: Color.fromARGB(255, 126, 120, 202),
-            primaryColorDark: Color.fromARGB(255, 47, 44, 80),
-            cardColor: Color.fromARGB(255, 47, 44, 80),
-            accentColor: Colors.green),
+            backgroundColor: Color.fromARGB(255, 58, 58, 126),
+            primaryColorDark: Color.fromARGB(255, 55, 47, 139),
+            cardColor: Color.fromARGB(255, 118, 107, 230),
+            accentColor: Color.fromARGB(255, 119, 76, 175)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Tak Sendiri'),
@@ -43,72 +40,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-  int idUser = 1;
-  String name = "Budi";
-  String prompt = "Med Stud Games";
-  String newChat = "";
-
-  final textPrompt = TextEditingController();
-
-  List<ChatMessage> messages = [
-    ChatMessage(
-        messageContent: "Halo! Apa yang bisa saya bantu?",
-        messageType: "receiver"),
-  ];
-
-  final ScrollController _controller = ScrollController();
-
-  void _scrollDown() {
-    _controller.animateTo(
-      _controller.position.maxScrollExtent,
-      duration: Duration(seconds: 2),
-      curve: Curves.fastOutSlowIn,
+  void _pushPage(BuildContext context, Widget page) {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(builder: (_) => page),
     );
   }
 
-  void promptTrigger() {
-    if (textPrompt.text == "") {
-    } else {
-      prompt = textPrompt.text;
-      setState(() {
-        newChat = prompt;
-        messages
-            .add(ChatMessage(messageContent: newChat, messageType: "sender"));
-
-        textPrompt.clear();
-      });
-      _scrollDown();
-      http
-          .post(Uri.parse("http://4.156.87.67/api/prompts"),
-              headers: {'Content-type': 'application/json'},
-              body: json.encode({"user_id": idUser, "prompt": prompt}))
-          .then((response) {
-        print(response.body);
-        // print("pokemon");
-        setState(() {
-          newChat = json.decode(response!.body)['response'];
-          messages.add(
-              ChatMessage(messageContent: newChat, messageType: "receiver"));
-
-          _scrollDown();
-        });
-      });
-    }
-  }
-
-  String extractLink(String input) {
-    var elements = linkify(input,
-        options: LinkifyOptions(
-          humanize: false,
-        ));
-    for (var e in elements) {
-      if (e is LinkableElement) {
-        return e.url;
-      }
-    }
-    return "no link";
-  }
+  int screenIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -117,212 +55,105 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Stack(
-        children: <Widget>[
-          Scrollbar(
-            child: ListView.builder(
-              controller: _controller,
-              itemCount: messages.length,
-              shrinkWrap: true,
-              padding: EdgeInsets.only(top: 10, bottom: 100),
-              // physics: NeverScrollableScrollPhysics(),
-              itemBuilder: (context, index) {
-                return Container(
-                  padding:
-                      EdgeInsets.only(left: 14, right: 14, top: 10, bottom: 10),
-                  child: Align(
-                    alignment: (messages[index].messageType == "receiver"
-                        ? Alignment.topLeft
-                        : Alignment.topRight),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: (messages[index].messageType == "receiver"
-                            ? BorderRadius.only(
-                                topRight: Radius.circular(15.0),
-                                bottomLeft: Radius.circular(25.0),
-                                bottomRight: Radius.circular(15.0),
-                              )
-                            : BorderRadius.only(
-                                topLeft: Radius.circular(15.0),
-                                bottomLeft: Radius.circular(15.0),
-                                bottomRight: Radius.circular(25.0),
-                              )),
-                        color: (messages[index].messageType == "receiver"
-                            ? Colors.grey.shade200
-                            : Color.fromARGB(255, 47, 44, 80)),
-                      ),
-                      padding: EdgeInsets.all(16),
-                      child: extractLink(messages[index].messageContent) ==
-                              "no link"
-                          ? Text(
-                              messages[index].messageContent,
-                              style: TextStyle(
-                                  fontSize: 15,
-                                  color:
-                                      messages[index].messageType == "receiver"
-                                          ? Colors.black
-                                          : Colors.white),
-                            )
-                          : InkWell(
-                              child: Text(
-                                messages[index].messageContent,
-                                style: TextStyle(color: Colors.blue),
-                              ),
-                              onTap: () {
-                                launchUrl(Uri.parse(extractLink(
-                                    messages[index].messageContent)));
-                              },
-                            ),
-//                           Html(
-//                               data: messages[index].messageContent,
-
-//                               onLinkTap: (url, context, attributes, element) {
-//                                 // open url in a webview
-//                                 launchUrl(url! as Uri);
-//                               },
-// //
-//                               style: {
-//                                 "html": Style.fromTextStyle(
-//                                   TextStyle(fontSize: 14.0),
-//                                 ),
-//                               },
-//                             ),
-                    ),
-                  ),
-                );
-              },
+      body: Center(
+        child: Column(children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 100),
+            child: Image.asset('images/logo.png',
+                color: Color.fromRGBO(255, 255, 255, 0.5),
+                colorBlendMode: BlendMode.modulate,
+                height: 145,
+                width: 145),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 30),
+            child: Text(
+              "Kamu tidak sendiri",
+              style: TextStyle(
+                  // backgroundColor: Color.fromRGBO(74, 57, 119, 0.494),
+                  color: Colors.white,
+                  fontSize: 25),
+              textAlign: TextAlign.center,
             ),
           ),
-          Align(
-            alignment: Alignment.bottomLeft,
-            child: Container(
-              padding: EdgeInsets.only(left: 10, bottom: 10, top: 10),
-              height: 60,
-              width: double.infinity,
-              color: Colors.white,
-              child: Row(
+          Container(
+            padding: EdgeInsets.only(bottom: 20, left: 30, right: 30),
+            child: Text(
+              "Semua perasaan kamu nyata dan kita menerima perasaanmu.",
+              style: TextStyle(
+                  // backgroundColor: Color.fromRGBO(74, 57, 119, 0.494),
+                  color: Colors.white,
+                  fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Stack(
                 children: <Widget>[
-                  // GestureDetector(
-                  //   onTap: () {},
-                  //   child: Container(
-                  //     height: 30,
-                  //     width: 30,
-                  //     decoration: BoxDecoration(
-                  //       color: Color.fromARGB(255, 47, 44, 80),
-                  //       borderRadius: BorderRadius.circular(30),
-                  //     ),
-                  //     child: Icon(
-                  //       Icons.clear,
-                  //       color: Colors.white,
-                  //       size: 20,
-                  //     ),
-                  //   ),
-                  // ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  Expanded(
-                    child: TextField(
-                      controller: textPrompt,
-                      decoration: InputDecoration(
-                          hintText: "Ketik di sini...",
-                          hintStyle: TextStyle(color: Colors.black54),
-                          border: InputBorder.none),
-                      onEditingComplete: () {
-                        promptTrigger();
-                        FocusScope.of(context).unfocus();
-                      },
-                    ),
-                  ),
-                  SizedBox(
-                    width: 15,
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {
-                      promptTrigger();
-                    },
-                    child: Icon(
-                      Icons.send,
-                      color: Colors.white,
-                      size: 18,
-                    ),
-                    backgroundColor: Color.fromARGB(255, 47, 44, 80),
-                    elevation: 0,
-                    shape: CircleBorder(
-                        // borderRadius: BorderRadius.all(Radius.circular(15.0))
+                  Positioned.fill(
+                    child: Container(
+                      padding: EdgeInsets.only(top: 40, bottom: 40),
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          tileMode: TileMode.decal,
+                          colors: <Color>[
+                            Color.fromARGB(255, 255, 255, 255),
+                            Color.fromARGB(255, 200, 201, 232),
+                            Color.fromARGB(255, 174, 157, 169),
+                          ],
                         ),
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: Color.fromARGB(255, 50, 22, 100),
+                      padding: const EdgeInsets.all(16.0),
+                      textStyle: const TextStyle(fontSize: 22),
+                    ),
+                    onPressed: () {
+                      _pushPage(context, chatbot());
+                    },
+                    child: const Text(
+                      '▶️ Mulai Chat',
+                      textAlign: TextAlign.center,
+                    ),
                   ),
                 ],
               ),
             ),
           ),
+        ]),
+      ),
+      bottomNavigationBar: SalomonBottomBar(
+        backgroundColor: Colors.white,
+        selectedItemColor: Color.fromARGB(255, 92, 51, 124),
+        currentIndex: screenIndex,
+        onTap: clickBottomNav,
+        items: [
+          SalomonBottomBarItem(
+            icon: Icon(
+              Icons.play_circle_filled,
+            ),
+            title: Text('Home'),
+          ),
+          SalomonBottomBarItem(
+            icon: Icon(
+              Icons.settings,
+            ),
+            title: Text('Settings'),
+          ),
         ],
       ),
     );
+  }
 
-    // return Scaffold(
-    //   appBar: AppBar(
-    //     backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-    //     title: Text(widget.title),
-    //   ),
-    //   body: Center(
-    //     child: Column(
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         Scrollbar(
-    //           child: SingleChildScrollView(
-    //             child: Column(
-    //               mainAxisSize: MainAxisSize.min,
-    //               children: <Widget>[
-    //                 FutureBuilder(
-    //                     future: http.post(
-    //                         Uri.parse("http://4.156.87.67/api/prompts"),
-    //                         headers: {'Content-type': 'application/json'},
-    //                         body: json
-    //                             .encode({"user_id": idUser, "prompt": prompt})),
-    //                     builder: (context, snapshot) {
-    //                       if (snapshot.hasData) {
-    //                         print(snapshot.data);
-    //                         return Column(
-    //                           children: <Widget>[
-    //                             RichText(
-    //                               text: TextSpan(
-    //                                 text: "Respone:/n" +
-    //                                     json.decode(
-    //                                         snapshot.data!.body)['response'],
-    //                                 style: TextStyle(
-    //                                   fontSize: 16,
-    //                                   color: Colors.black,
-    //                                   fontWeight: FontWeight.normal,
-    //                                 ),
-    //                               ),
-    //                             ),
-    //                           ],
-    //                         );
-    //                       } else if (snapshot.hasError) {
-    //                         return Text(
-    //                           "[!].",
-    //                           textAlign: TextAlign.left,
-    //                           style: new TextStyle(
-    //                               fontSize: 16.0,
-    //                               fontWeight: FontWeight.normal),
-    //                         );
-    //                       } else {
-    //                         return Center(child: CircularProgressIndicator());
-    //                       }
-    //                     }),
-    //               ],
-    //             ),
-    //           ),
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    //   // floatingActionButton: FloatingActionButton(
-    //   //   onPressed: _incrementCounter,
-    //   //   tooltip: 'Increment',
-    //   //   child: const Icon(Icons.add),
-    //   // ), // This trailing comma makes auto-formatting nicer for build methods.
-    // );
+  void clickBottomNav(int index) {
+    setState(() {
+      screenIndex = index;
+    });
   }
 }
